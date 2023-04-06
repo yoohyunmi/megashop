b7392f1ed755b4376ac06b421351eaf4350387c7
유현미 <hyum@yuhyeonmiui-MacBookPro.local>
2023-04-06 23:34:00 +0900

    Initial commit

---
+++ src/main/java/minmi/side/MinmishopApplication.java
@@ -0,0 +1,13 @@
+package minmi.side;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinmishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinmishopApplication.class, args);
+	}
+
+}
