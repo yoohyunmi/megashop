61203d6f3189e027ab1ebf9e44f3016cb2b3b406
hyum <yoohm3@naver.com>
2023-04-07 14:08:12 +0900

    Refactor name and Applied Boot strap

---
+++ src/main/java/mega/side/MegashopApplication.java
@@ -0,0 +1,13 @@
+package mega.side;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegashopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mega/side/MegashopApplicationTests.java
@@ -0,0 +1,13 @@
+package mega.side;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
