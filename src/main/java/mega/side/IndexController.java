9d35cc9520f2e30ae8e75fb195130d7733e2ff7c
hyum <yoohm3@naver.com>
2023-04-12 16:48:31 +0900

    Applied ModelAndView

--- src/main/java/mega/side/IndexController.java
+++ src/main/java/mega/side/IndexController.java
@@ -2,12 +2,16 @@ package mega.side;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
     
     @GetMapping("/index")
-    public String index() {
-        return "index";
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        
+        return mv;
     }
 }
