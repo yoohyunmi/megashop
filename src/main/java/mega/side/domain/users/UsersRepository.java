94b7cf799b97dfca1cf6b0c18a2d00c8975fca0b
yoohyunmi <yoohm3@naver.com>
2023-04-16 01:24:23 +0900

    JPA 적용 및 user 테이블 insert 테스트코드

---
+++ src/main/java/mega/side/domain/users/UsersRepository.java
@@ -0,0 +1,7 @@
+package mega.side.domain.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<Users, Integer> {
+
+}
---
+++ src/test/java/mega/side/domain/users/UsersRepositoryTest.java
@@ -0,0 +1,45 @@
+package mega.side.domain.users;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UsersRepositoryTest {
+    @Autowired
+    UsersRepository usersRepository;
+
+    @AfterEach
+    public void cleanup() {
+        usersRepository.deleteAll();
+    }
+
+    @Test
+    public void 회원저장_불러오기() {
+        // given
+        String name = "user01";
+        String password = "password";
+
+        usersRepository.save(Users.builder()
+                .name(name)
+                .password(password)
+                .email("user@gmail.com")
+                .build());
+        // when
+        List<Users> usersList = usersRepository.findAll();
+
+        // then
+        Users users = usersList.get(0);
+
+        assertThat(users.getName()).isEqualTo(name);
+        assertThat(users.getPassword()).isEqualTo(password);
+    }
+}
